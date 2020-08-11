cfbca88a85ffafbbc30d93b44426d1461cfd9b45
Alexandre Diniz <32992263+Alexandre-Diniz@users.noreply.github.com>
2020-08-11 00:41:16 -0300

    Create check-phrase-includes-words.js

---
+++ check-phrase-includes-words.js
@@ -0,0 +1,8 @@
+const verify = (phrase, words) => {
+  const contain = words.split(' ').map(word => {
+    if (phrase.toLowerCase().includes(word)) {
+      return true
+    } else return false
+  })
+  return contain.every(item => item === true)
+}
